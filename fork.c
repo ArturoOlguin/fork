6e388487741d5483d0a67462c697e5c099a7755b
ArturoOlguin <158232301+ArturoOlguin@users.noreply.github.com>
2024-03-29 17:54:30 -0600

    Update fork.c
    Funcion para calcular caracteres y modulo

--- fork.c
+++ fork.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
 
+/*prototipos*/
+
+int contarCaracteres();
+
+int main() {
+    printf("Ingresa tu nombre:\n");
+    int cantidadCaracteres = contarCaracteres();
+    printf("La cantidad de caracteres ingresados es: %d\n", cantidadCaracteres);
+    printf ("El arbol asignado es: %d", cantidadCaracteres%2);
+
+    return 0;
+}
+
+
+int contarCaracteres() {
+    int c = getchar();
+    
+    if (c == EOF || c == '\n') {
+        return 0;
+    } 
+    else {
+        return 1 + contarCaracteres();
+    }
+}
